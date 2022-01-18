a3a9d26719dd2c955307cf78ab98d6b835e0e68d
Mathias <mathias_sn@hotmail.com>
2022-01-18 21:45:37 +0100

    feat: added tutorial custom enchantment 'Frost', removed 'Example' mod and mixin

---
+++ src/main/java/net/fabricmc/extendedenchantments/ExtendedEnchantments.java
@@ -0,0 +1,33 @@
+package net.fabricmc.extendedenchantments;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.extendedenchantments.enchantment.FrostEnchantment;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ExtendedEnchantments implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LogManager.getLogger("extendedenchantments");
+
+	// what's the point of assigning if we're not using it...?
+	private static Enchantment FROST = Registry.register(
+            Registry.ENCHANTMENT,
+            new Identifier("extendedenchantments", "frost"),
+            new FrostEnchantment()
+    );
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world from Extended Enchantments!");
+	}
+}
